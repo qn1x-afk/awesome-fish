35ac9f67c4f739be60ae8cc8b507a7efc55b2b60
Evan Julius <evanjulius66@yahoo.com>
2026-01-28 13:27:59 -0600

    feat: adding installer to main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from helpers.installer import install
+from dotenv import load_dotenv
+load_dotenv()
 
 def main(*args, **kwargs):
+    from helpers.installer import install
     install(*args, **kwargs)
 
 if __name__ == "__main__":
