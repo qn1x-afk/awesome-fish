d12085c3cec9e9bc35f190dd5c2a04ca96016c89
Evan Julius <evanjulius66@yahoo.com>
2026-01-28 13:00:46 -0600

    Fix: refactoring helper functions into their own dir

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import os
+from helpers.installer import install
 
-def main():
-    # Get the current working directory
-    current_directory = os.getcwd()
-    
-    # List all files and directories in the current directory
-    items = os.listdir(current_directory)
-    
-    # Print the items
-    print("Items in the current directory:")
-    for item in items:
-        print(item)
-
+def main(*args, **kwargs):
+    install(*args, **kwargs)
 
 if __name__ == "__main__":
     main()
